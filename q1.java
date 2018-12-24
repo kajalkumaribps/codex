6d07b5f0cdd14ab51a00c48a015471f6e62e19db
kajalkumaribps <45880658+kajalkumaribps@users.noreply.github.com>
2018-12-24 10:57:04 +0530

    Update q1.java

--- q1.java
+++ q1.java
@@ -1,9 +1,9 @@
-import java.util.Scanner;
+
 public class q1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Scanner sc=new Scanner (System.in);
+
 int n=1000;
 int sum=0;
 for(int i=1;i<1000;i++)
