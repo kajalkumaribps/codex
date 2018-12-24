5220f1bb19a9ffd2fbd3127bd812c9014a3430b7
kajalkumaribps <45880658+kajalkumaribps@users.noreply.github.com>
2018-12-24 15:48:12 +0530

    Update q7.java

--- q7.java
+++ q7.java
@@ -3,32 +3,31 @@ public class q7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	 long c1=0,i=2,prime=0;
+		 int ctr=0;
+		 while(i>0)
+		 {
+			 for(long k=1;k<=Math.sqrt(i);k++)
+			 {
+				 if(i%k==0)
+				 {
+					 ctr++;
+				 }
+			 }
+			 if(ctr==1)
+			   c1++;
+			  if(c1==10001)
+			 {
+				 prime=i;
+				 break;
+			 }
+			 i++;
+			 ctr=0;
+		 }
+		 System.out.println("10001st prime is:"+prime);
 
-int c=1;
-int i=3;
-int prime;
-while(c<=10001)
-{
-	int ctr=0;
-	for(int k=2;k<i;k++)
-	{
-		if(i%k==0)
-		{
-			ctr++;
-			break;
-		}
-	}
-		if(ctr==0)
-		{
-			prime=i;
-			c++;
-			if(c==10001)
-			{
-				System.out.println("prime number at 10001st position is ="+prime);
-			}
-		}
-	i=i+2;
-}
+
+
 	}
 
 }
