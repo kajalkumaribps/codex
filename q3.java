5c431cf4f996551a625cbde7e9252bb7bcd706dc
kajalkumaribps <45880658+kajalkumaribps@users.noreply.github.com>
2018-12-24 14:13:53 +0530

    Update q3.java

--- q3.java
+++ q3.java
@@ -5,32 +5,22 @@ public class q3 {
 		// TODO Auto-generated method stub
 
 long  n= 600851475143L;
- long m=n;
  long mxpr=0;
- if(n%2==0)
- { mxpr=2;}
- for(long i=3;i<=n;i=i+2)
+while(n%2==0)
+{
+	mxpr=2;
+	n=n/2;
+}
+ for(long i=3;i<=Math.sqrt(n);i=i+2)
 {
 if(n%i==0)
 	{
-	long c=0;
-	for(long j=2;j<=Math.sqrt(i);j++)
-	{
-		if(i%j==0)
-		{
-		c=1;
-		break;
-		}
-	}
-	if(c==0)
-	{
-		n/=i;
-		if(mxpr<i)
-			mxpr=i;
-	}
+	n=n/i;
+	mxpr=n;
 	}
 }
-System.out.println("largest prime factor of the number "+m+ "="+mxpr);
- }
+ if(n>2)
+System.out.println("largest prime factor of the number ="+mxpr);
+}
 
 }
