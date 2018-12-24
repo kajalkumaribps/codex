0227d01a8c324f9c39d517da6da1870d6f9f850b
kajalkumaribps <45880658+kajalkumaribps@users.noreply.github.com>
2018-12-24 17:57:43 +0530

    Update q5.java

--- q5.java
+++ q5.java
@@ -1,16 +1,23 @@
 
-public class q5 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-int i=2520;
-while(i%1!=0 || i%2!=0 || i%3!=0 || i%4!=0 || i%5!=0 || i%6!=0 ||  i%7!=0 || i%8!=0 || i%9!=0 || i%10!=0 ||
-      i%11!=0 || i%12!=0   || i%13!=0 || i%14!=0 || i%15!=0 || i%16!=0 || i%17!=0 || i%18!=0 || i%19!=0 || i%20!=0)
-		{
-				i++;
-		}
-
-		System.out.println("The smallest positive number that is evenly divisible by all of the number from 1 to 20 ="+i);
-		}
+       public class q5 {
+         static long gcd(long a, long b) 
+	{ 
+	   if(a%b != 0)  
+	      return gcd(b,a%b); 
+	   else 
+	      return b; 
+	} 
+	 static long lcm(long n) 
+	{ 
+	    long c = 1;     
+	    for (long i = 1; i <= n; i++) 
+	        c = (c * i)/(gcd(c, i)); 
+	    return c; 
+	} 
+	 public static void main(String []args)  
+	{ 
+	    long n = 20; 
+	    System.out.println("The smallest positive number that is evenly divisible by all of the number from 1 to 20 ="+lcm(n));
+	  }
 
 }
