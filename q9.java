15c0aaf26a439147a4b2fb98b15b5abe4fc3fc77
kajalkumaribps <45880658+kajalkumaribps@users.noreply.github.com>
2018-12-24 14:31:39 +0530

    Update q9.java

--- q9.java
+++ q9.java
@@ -2,29 +2,19 @@ public class q9 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int a,b,c,flag=1;
-int pro=0;
-a=1;  b=2;  c=3;
-while(c<997 && flag==1)
-{b=2;
-while(b<c && flag==1)
-{a=1;
-	while(a<b && flag==1)
-	{
-		if((a*a)+(b*b)==(c*c))
+	 for(int a=3;a<=1000;a++)
 		{
-			if(a+b+c==1000)
+			for(int b=a+1;b<1000;b++)
 			{
-				pro=(a*b)*c;
-				System.out.println("product of the pythagorean triplet="+pro);
-			}
-		}
-		a++;
-	}
-	b++;
-}
-c++;
-}
-	}
-
+				double sum=Math.pow (a,2)+Math.pow(b,2);
+                                double c=1000-a-b;
+                                double d=Math.pow (c,2);
+                                
+                                if (d==sum)
+                                {
+                                	System.out.println("product of the pythagorean triplet="+(int)(a*b*c));
+                                   break;
+                                }
+			} 
+  }
 }
